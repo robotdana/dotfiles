0d0b8a556d14a58149c4e57d0607f0f107111f6b
Dana Sherson <robot@dana.sh>
2017-02-22 10:08:49 +1100

    use new verticals file structure to allow easier script/console-ing

--- marketplacer.sh
+++ marketplacer.sh
@@ -2,18 +2,30 @@ export VERTICAL='bikeexchange'
 
 alias jslint='./node_modules/.bin/eslint --quiet --ext .jsx,.js webpack/app webpack/entry webpack/lib webpack/test webpack/vendor'
 
-alias missing_verticals='comm -13 <(printf "%s\n" $VERTICALS | cut -d: -f2 | sort | uniq) <(marketplacer verticals | sort)'
+alias missing_verticals='comm -13 <(verticals | colrm 1 10 | sort | uniq) <(marketplacer verticals | sort)'
 
 function short_vertical() {
+  vertical_row $* | colrm 5 | tr -d " "
+}
+function long_vertical() {
+  vertical_row $* | colrm 1 10
+}
+
+function vertical_row() {
   if (($# == 1)); then
     local vertical=$1
   else
     local vertical=$VERTICAL
   fi
-  printf '%s\n' $VERTICALS | grep ":$vertical$" | cut -d: -f1 | head -n 1
+  verticals | grep -e "^$vertical\|$vertical$" | head -n 1
 }
-function long_vertical() {
-  printf '%s\n' $VERTICALS | grep "^$1:" | cut -d: -f2 | head -n 1
+
+function vertical_server() {
+  vertical_row $* | colrm 1 5 | colrm 5
+}
+
+function verticals() {
+  cat ~/.dotfiles/locals/verticals
 }
 
 function v(){
@@ -30,13 +42,11 @@ function vv() {
 
 function vdb() {
   (( $# == 1 )) && v $1
-  title 'Updating Database'
   yes | m database update $VERTICAL
-  (rails db:migrate index:reindex) && echo '••• Migrated and reindexed •••'
 }
-
-function update_db() {
+function vdbi() {
   vdb $*
+  rails db:migrate
 }
 
 function vdbrs() {
@@ -50,20 +60,36 @@ function vdbrc() {
 }
 
 function vrc() {
-  (( $# == 1 )) && v $1
-  rc
+  (( $# > 0 )) && v $1
+  if (( $# == 2 )); then
+    remote_console $2
+  else
+    rc
+  fi
 }
+
+function remote_console() {
+  if [ "$1" = "prod" ]; then
+    local server=$(vertical_server)
+  elif [ "$1" = "staging" ]; then
+    local server=$(vertical_server)-staging
+  else
+    local server=$1
+  fi
+  $MARKETPLACER_PATH/script/console $server $VERTICAL
+}
+
 function vrs() {
   if (( $# == 1 )); then
     v $1
     vrs
   elif (( $# == 0 )); then
-    rfs $(m verticals | awk "/$VERTICAL/{ print NR-1; exit }") $VERTICAL
+    rfs $(verticals | colrm 1 10 | awk "/$VERTICAL/{ print NR-1; exit }") $VERTICAL
   else
     local current_dir=$PWD
     for vertical in "$@"
     do
-      tab "cd $current_dir && vrs $vertical"
+      ttab -G "cd $current_dir && vrs $vertical"
     done
   fi
 }

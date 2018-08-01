eab13f14f80dd0047e4ab86d075ae1abfef1be79
Dana Sherson <robot@dana.sh>
2018-08-01 15:01:17 +1000

    fix cc_menu_remove

--- functions/marketplacer_support.sh
+++ functions/marketplacer_support.sh
@@ -93,7 +93,7 @@ function cc_menu_add {
 function cc_menu_remove {
   local branch="${1:-$(git_current_branch)}"
   if cc_menu_present "$branch"; then
-    cc_menu_initialize $(cc_menu_list -e $branch)
+    cc_menu_initialize $(cc_menu_list $branch)
   fi
 }
 
@@ -103,11 +103,14 @@ function cc_menu_remove_purged {
 
 function cc_menu_add_item {
   local branch="$1"
+  local project_name=${2:-Marketplacer - remote ($branch)}
+  local server_url=${3:-https://cc.buildkite.com/marketplacer/marketplacer-remote.xml?access_token=$CC_BUILDKITE_TOKEN&branch=$branch}
+
   defaults write net.sourceforge.cruisecontrol.CCMenu Projects -array-add "
     {
-      displayName = $branch;
-      projectName = \"Marketplacer - remote ($branch)\";
-      serverUrl = \"https://cc.buildkite.com/marketplacer/marketplacer-remote.xml?access_token=$CC_BUILDKITE_TOKEN&branch=$branch\";
+      displayName = \"$branch\";
+      projectName = \"$project_name\";
+      serverUrl = \"$server_url\";
     }
   "
 }
@@ -120,16 +123,9 @@ function cc_menu_present {
 function cc_menu_initialize {
   killall CCMenu
   defaults write net.sourceforge.cruisecontrol.CCMenu Projects '()'
-  defaults write net.sourceforge.cruisecontrol.CCMenu Projects -array-add "{
-      displayName = \"- deploys -\";
-      projectName = deploys;
-      serverUrl = \"https://cc.buildkite.com/marketplacer/deploys.xml?access_token=$CC_BUILDKITE_TOKEN\";
-  }"
-  defaults write net.sourceforge.cruisecontrol.CCMenu Projects -array-add "{
-      displayName = \"- master -\";
-      projectName = \"Marketplacer - remote (master)\";
-      serverUrl = \"https://cc.buildkite.com/marketplacer/marketplacer-remote.xml?access_token=$CC_BUILDKITE_TOKEN&branch=master\";
-  }"
+  cc_menu_add_item deploys deploys "https://cc.buildkite.com/marketplacer/deploys.xml?access_token=$CC_BUILDKITE_TOKEN"
+  cc_menu_add_item master
+  cc_menu_add_item "$(cc_menu_separator)" '' ''
   for branch in "$@"; do
     cc_menu_add_item "$branch"
   done
@@ -137,5 +133,9 @@ function cc_menu_initialize {
 }
 
 function cc_menu_list {
-  defaults read net.sourceforge.cruisecontrol.CCMenu Projects | grep displayName | tr -d '",;' | colrm 1 22 | grep -vF -e master -e deploys -e ---
+  defaults read net.sourceforge.cruisecontrol.CCMenu Projects | grep displayName | tr -d '",;' | colrm 1 22 | grep -vF -e master -e deploys -e "$(cc_menu_separator)" "${@/#/-e }"
+}
+
+function cc_menu_separator {
+  echo "----------------------------------------------"
 }

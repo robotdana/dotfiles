2b0d4f255859684a21a8c40b5b12f6f507578bfc
Dana Sherson <robot@dana.sh>
2018-11-01 14:43:00 +1100

    Add git last merge

--- functions/git_support.sh
+++ functions/git_support.sh
@@ -367,3 +367,7 @@ function github_file {
 function github_file_master {
   open $(github_path)/tree/master/$1
 }
+
+function git_last_merge {
+  git show $(git log --format="%h^" master..HEAD | tail -n 1 ) --format=%cr | grep day
+}

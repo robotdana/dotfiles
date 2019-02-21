f5ba4b6f41d1a1c3b93e15b5fda574065d1ba830
Dana Sherson <robot@dana.sh>
2019-02-21 14:55:55 +1100

    Add a reinstall_ruby function
    So i don't have to remember all the steps

--- functions/rails_support.sh
+++ functions/rails_support.sh
@@ -19,3 +19,22 @@ function rails_migrate_soft {
   SKIP_SCHEMA_DUMP=1 echodo bundle exec rails db:migrate
   mv db/schema.rb.bak db/schema.rb
 }
+
+function reinstall_ruby {
+  [[ -z "$(type chruby 2>/dev/null)" ]] && echoerr "chruby is missing" && return 1
+  which -s ruby-install || ( echoerr "ruby-install is missing" && return 1 )
+
+  ruby_path=$(which ruby)
+
+  if [[ ! "$ruby_path" = *.rubies* ]]; then
+    echoerr "existing ruby isn't installed with chruby" && return 1
+  fi
+
+  chruby_identifier=$(chruby | grep '\*' | cut -d' ' -f3)
+
+  rm -rf "${ruby_path%/bin/ruby}"
+  ruby-install "$chruby_identifier"
+  bundler_version=$(tail -n 1 Gemfile.lock)
+  gem install bundler "${bundler_version/#/--version=}"
+  bundle
+}

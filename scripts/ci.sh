4119ea09a23567094db79d128b639844b9b48d6b
Dana Sherson <robot@dana.sh>
2023-12-04 23:24:31 +1300

    What if i do this

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,5 +3,6 @@
 # we assume this is checked out to ~/.dotfiles
 ln -s $PWD ~/.dotfiles
 ~/.dotfiles/scripts/install.sh
+. ~/.bash_profile
 git config --global commit.gpgsign false
-bash -lc 'bundle exec rspec'
+bundle exec rspec

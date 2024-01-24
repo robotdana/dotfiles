9dd0a496c4b2bd9227d5244b5c868c5f614ba4e0
Dana Sherson <robot@dana.sh>
2024-01-25 10:31:26 +1300

    Remove ttab

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,15 +8,6 @@ function git_version_number {
   git --version | cut -d\( -f1 | grep -oE "[0-9\.]+"
 }
 
-# get ttab
-echodo curl https://raw.githubusercontent.com/mklement0/ttab/stable/bin/ttab > ~/.dotfiles/locals/ttab
-if [[ $(wc -l ~/.dotfiles/locals/ttab | awk -F' ' '{print $1}') = "1" ]]; then
-  echoerr "ttab didn't download correctly"
-  exit 1
-fi
-echodo chmod +x ~/.dotfiles/locals/ttab
-echodo ln -sf ~/.dotfiles/locals/ttab /usr/local/bin/ttab
-
 # get git-completion
 echodo curl "https://raw.githubusercontent.com/git/git/v$(git_version_number)/contrib/completion/git-completion.bash" > ~/.dotfiles/locals/git-completion.bash
 if [[ $(wc -l ~/.dotfiles/locals/git-completion.bash | awk -F' ' '{print $1}') = "1" ]]; then
